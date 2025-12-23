9fb52112e568ea3b8533e53d302ae9ff47f3e1b0
Igor Bugaytsov <i.bugaytsov@mail.ru>
2025-12-24 01:09:26 +0300

    Marvel_project

---
+++ src/components/app/App.js
@@ -0,0 +1,14 @@
+
+import Header from '../header/Header';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
